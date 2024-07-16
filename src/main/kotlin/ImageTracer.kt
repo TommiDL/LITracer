86cf95d633c0cc556b925dd23b69d1def05fc41f
Tommaso Di Luciano <tommaso.diluciano@studenti.unimi.it>
2024-07-16 16:10:21 +0200

    antialiasing implementation

--- src/main/kotlin/ImageTracer.kt
+++ src/main/kotlin/ImageTracer.kt
@@ -23,11 +23,13 @@ class ImageTracer(val image:HdrImage, val camera: Camera)
     }
 
     /**
-     * Fires a ray in every pixel of the image with a given function for rendering the ray
+     * Fires a 'samples' number of rays using stratified sampling in every pixel of the image with a given function for rendering the ray
      * @func = function that takes a Ray and returns a Color
      */
-    fun fire_all_ray(func:(Ray)->Color)
+    fun fire_all_ray(func:(Ray)->Color, pcg: PCG=PCG(), samples:Int=1)
     {
+        val side:Float=1f/samples
+
         var counter:Int=0
         val size:Int=this.image.height*this.image.width
 
@@ -38,8 +40,16 @@ class ImageTracer(val image:HdrImage, val camera: Camera)
         {
             for(col in  0 until this.image.width)
             {
-                val ray:Ray=this.fire_ray(col, row)
-                val color:Color=func(ray)
+                var color:Color=Color()
+
+                for (i in 0 until  samples)
+                {
+                    val ray:Ray=this.fire_ray(col, row, u_pixel = i*side+pcg.random_float(), v_pixel = i*side+pcg.random_float())
+
+                    color+=func(ray)
+
+                }
+                color/=samples
 
                 this.image.set_pixel(col, row, color)
 
